cb2845b5b731e04a37989f255b5f766da1061136
znol1 <57703588+znol1@users.noreply.github.com>
2021-04-07 18:48:19 +0300

    first model

--- coworking/models.py
+++ coworking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    location = models.TextField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
