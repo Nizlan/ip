3851a68b6d8b9cf604621539a5ac6dcc72f2cbed
znol1 <57703588+znol1@users.noreply.github.com>
2021-04-07 23:31:43 +0300

    fixes

--- coworking/admin.py
+++ coworking/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Place
+from .models import Place, Categories
 
 admin.site.register(Place)
+admin.site.register(Categories)
--- coworking/models.py
+++ coworking/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
 
 class Place(models.Model):
-    name = models.CharField(max_length=150)
-    description = models.TextField()
-    location = models.TextField()
-    price = models.FloatField()
+    name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    location = models.TextField(verbose_name='Местоположение')
+    price = models.FloatField(verbose_name='Цена, р')
+    categories = models.ForeignKey('Categories', null=True, on_delete=models.PROTECT, verbose_name='Категория')
 
     def __str__(self):
         return self.name
@@ -15,3 +16,15 @@ class Place(models.Model):
     class Meta:
         verbose_name = 'Место'
         verbose_name_plural = 'Места'
+
+
+class Categories(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
