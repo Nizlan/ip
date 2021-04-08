81c458a6050e369f3bc52b2fa696f1d93d34ffc9
znol1 <57703588+znol1@users.noreply.github.com>
2021-04-08 21:07:44 +0300

    some models added, inline added to booked

--- coworking/admin.py
+++ coworking/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 
-from .models import Place, Categories, Services
+from .models import Place, Categories, Services, AvailableTime, BookedTime
 
-admin.site.register(Place)
+
+class BookedTimeInline(admin.TabularInline):
+    model = BookedTime
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [BookedTimeInline]
+
+
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(Categories)
 admin.site.register(Services)
+admin.site.register(AvailableTime)
+admin.site.register(BookedTime)
--- coworking/models.py
+++ coworking/models.py
@@ -10,6 +10,7 @@ class Place(models.Model):
     price = models.FloatField(verbose_name='Цена, р')
     categories = models.ForeignKey('Categories', null=True, on_delete=models.PROTECT, verbose_name='Категория')
     services = models.ManyToManyField('Services')
+    available_time = models.ForeignKey('AvailableTime', null=True, on_delete=models.PROTECT, verbose_name='Время работы')
 
     def __str__(self):
         return self.name
@@ -40,3 +41,32 @@ class Services(models.Model):
     class Meta:
         verbose_name = 'Услуга'
         verbose_name_plural = 'Услуги'
+
+
+class AvailableTime(models.Model):
+    is_round_the_clock = models.BooleanField(null=True, verbose_name='Круглосуточно')
+    regular_start = models.TimeField(null=True, verbose_name='Начало работы')
+    regular_end = models.TimeField(null=True, verbose_name='Конец работы')
+    regular_start2 = models.TimeField(null=True, verbose_name='Начало работы в специальные дни')
+    regular_end2 = models.TimeField(null=True, verbose_name='Конец работы в специальные дни')
+    days_for_regular2 = models.CharField(max_length=150, verbose_name='Специальные дни')
+
+    def __str__(self):
+        return f'{self.regular_start} - {self.regular_end}'
+
+    class Meta:
+        verbose_name = 'Время работы'
+        verbose_name_plural = 'Времена работы'
+
+
+class BookedTime(models.Model):
+    start = models.TimeField(null=True, verbose_name='Начало работы')
+    end = models.TimeField(null=True, verbose_name='Конец работы')
+    place = models.ForeignKey('Place', null=True, on_delete=models.CASCADE, verbose_name='Место')
+
+    def __str__(self):
+        return f'{self.start} - {self.end}'
+
+    class Meta:
+        verbose_name = 'Забронированное время'
+        verbose_name_plural = 'Забронированные часы'
