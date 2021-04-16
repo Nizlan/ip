f1b0cc542bc49d2a0a32246c2c933a02b6b1b72f
znol1 <57703588+znol1@users.noreply.github.com>
2021-04-16 22:48:28 +0300

    added classes to services

--- services/admin.py
+++ services/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import FreeServices
+from .models import FreeServices, Equipment, UniqueQualities
 
 
 admin.site.register(FreeServices)
+admin.site.register(Equipment)
+admin.site.register(UniqueQualities)
--- services/models.py
+++ services/models.py
@@ -14,3 +14,28 @@ class FreeServices(models.Model):
     class Meta:
         verbose_name = 'Услуги'
         verbose_name_plural = 'Услуги'
+
+
+class Equipment(models.Model):
+    area = models.FloatField(verbose_name="Площадь, м2")
+    free_spaces = models.IntegerField(verbose_name="Количество свободных мест")
+    conference_hall = models.BooleanField(verbose_name="Наличие конференц зала")
+
+    def __str__(self):
+        return self.area
+
+    class Meta:
+        verbose_name = 'Оснащение'
+        verbose_name_plural = 'Оснащения'
+
+
+class UniqueQualities(models.Model):
+    quality = models.CharField(max_length=150, verbose_name="Качество")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.quality
+
+    class Meta:
+        verbose_name = 'Уникальное качество'
+        verbose_name_plural = 'Уникальные качества'
